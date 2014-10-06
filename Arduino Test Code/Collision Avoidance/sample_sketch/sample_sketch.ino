afc1a7b70509eec2d4394ea36906b517c5b9b3c0
gagichce <gagichce@mcmaster.ca>
2014-10-05 22:49:38 -0400

    added the working sample code

---
+++ Arduino Test Code/Collision Avoidance/sample_sketch/sample_sketch.ino
@@ -0,0 +1,42 @@
+#define TRIGPIN 11   //Trig
+#define ECHOPIN 12   //Echo
+long duration, cm, inches;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(TRIGPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+}
+ 
+void loop()
+{
+ 
+ 
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(ECHOPIN, INPUT);
+  duration = pulseIn(ECHOPIN, HIGH);
+ 
+  // convert the time into a distance
+  cm = (duration/2) / 29.1;
+  inches = (duration/2) / 74; 
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+}
