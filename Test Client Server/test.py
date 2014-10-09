352e0ad5b97b170d4649135044cc7fc339674ba4
gagichce <gagichce@mcmaster.ca>
2014-10-09 16:12:53 -0400

    Threaded Server example working and tested with python client. Sends Hello world back and forth

--- Test Client Server/test.py
+++ Test Client Server/test.py
@@ -2,7 +2,7 @@ import socket
 
 HOST, PORT = "127.0.0.1", 5000
 
-sock = socket(AF_INET, SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect((HOST, PORT))
 
---
+++ kulturServer/kulturServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kulturServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server myServer = new Server();
+
+            Console.ReadLine();
+        }
+    }
+}
