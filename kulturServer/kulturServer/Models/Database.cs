582b78168dce9072d45beea39e372b2776ceef4d
gagichce <gagichce@mcmaster.ca>
2014-10-22 12:27:58 -0400

    entity framework now works again!

--- kulturServer/kulturServer/Models/Database.cs
+++ kulturServer/kulturServer/Models/Database.cs
@@ -16,5 +16,6 @@ namespace kulturServer.Models
 
         public DbSet<Image> Images { get; set; }
         public DbSet<iRobotCreate> Robots { get; set; }
+        public DbSet<TwitterAccount> TwitterAccounts { get; set; }
     }
 }
---
+++ kulturServer/kulturServer/Models/TwitterAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace kulturServer.Models
+{
+    class TwitterAccount
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public string consumerKey { get; set; }
+        public string consumerSecret { get; set; }
+        public string accessToken { get; set; }
+        public string accessTokenSecret { get; set; }
+
+        [Required]
+        public virtual iRobotCreate iRobotCreate { get; set; }
+
+        public DateTime TimeAdded { get; set; }
+    }
+}
