8feb821e85113001ac18a50e1765bd7158a58a94
gagichce <gagichce@mcmaster.ca>
2014-10-26 00:11:18 -0400

    added debug mode before moving code around

--- piCam/picam.py
+++ piCam/picam.py
@@ -1,20 +1,28 @@
-import io
-import time
-import picamera
-import RPi.GPIO as GPIO
+DEBUG = TRUE
+#DEBUG = FALSE
 
-PICTURE_INTERVAL_SECONDS = 60
+if(not DEBUG)
+	import io
+	import time
+	import picamera
+	import RPi.GPIO as GPIO
 
-class PictureTaker(object):
-    def __init__(self):
-        self.camera = picamera.PiCamera()
+	PICTURE_INTERVAL_SECONDS = 60
 
-    def takePhoto(self):
-        self.camera.capture("photos/"+str(int(round(time.time() * 1000)))+'pipic.jpg')
+	class PictureTaker(object):
+		def __init__(self):
+			self.camera = picamera.PiCamera()
 
-if __name__ == "__main__":
-    while True:
-        pT = PictureTaker()
-        pT.takePhoto()
-        time.sleep(PICTURE_INTERVAL_SECONDS)
-    
+		def takePhoto(self):
+			fileName = "photos/"+str(int(round(time.time() * 1000)))+'pipic.jpg'
+			self.camera.capture(fileName)
+			return fileName
+
+	if __name__ == "__main__":
+		while True:
+			pT = PictureTaker()
+			pT.takePhoto()
+			time.sleep(PICTURE_INTERVAL_SECONDS)
+			#Does the camera have to be turned off here? - does it remain on?
+else
+	print ""
