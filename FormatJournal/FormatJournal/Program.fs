7bb9e6e875b40bfa206d6a4a07b4d7936205c912
gagichce <gagichce@mcmaster.ca>
2015-03-15 19:33:11 -0400

    added .rtf and start of parser

--- FormatJournal/FormatJournal/Program.fs
+++ FormatJournal/FormatJournal/Program.fs
@@ -1,7 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.IO
+
+let readLines =
+    "TXT.rtf"
+    |> File.ReadAllLines
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printf "Opened File. Parsed Out."
     0 // return an integer exit code
