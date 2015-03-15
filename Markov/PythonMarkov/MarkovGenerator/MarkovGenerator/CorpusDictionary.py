c709399ce3904714f9f234587d7830a79a3e279b
Dominik <domkaukinen@gmail.com>
2015-03-15 16:18:50 -0400

    dictionary parsing done with python

---
+++ Markov/PythonMarkov/MarkovGenerator/MarkovGenerator/CorpusDictionary.py
@@ -0,0 +1,26 @@
+class CorpusDictionary:
+    def __init__(self, filePath, numChar):
+        self.corpus = ""
+        with open(filePath, 'r') as myFile:
+            self.corpus = myFile.read().replace('\n', '')
+        self.cleanCorpus()
+        self.numChar = numChar
+        self.keyCollection = []
+        self.dic = {}
+
+    def cleanCorpus(self):
+        self.corpus = self.corpus.replace("\r", "")
+        self.corpus = self.corpus.replace("\n", "")
+
+    def makeDictionary(self):
+        for i in range(0, len(self.corpus) - self.numChar):
+            tempArray = ""
+            for j in range(0, self.numChar):
+                tempArray += self.corpus[i + j]
+            self.dic.setdefault(tempArray, [])
+            self.dic[tempArray].append(self.corpus[i + self.numChar])
+
+    def toString(self):
+        for key in self.dic:
+            print(key + "  =>   " + self.dic[key])
+
--- Markov/PythonMarkov/MarkovGenerator/MarkovGenerator/MarkovGenerator.py
+++ Markov/PythonMarkov/MarkovGenerator/MarkovGenerator/MarkovGenerator.py
@@ -1,4 +1,27 @@
-class Markov(fileName):
-    this.filename = fileName
+from CorpusDictionary import *
 
-    def 
+class Markov:
+    def __init__(self, fileName, chainSize):
+        self.filename = fileName
+        self.chainsize = chainSize
+        self.corpus = CorpusDictionary(self.filename, self.chainsize)
+        self.isParsed = False
+
+    def parseDictionary(self):
+        print("______Creating Dictionary_______")
+        self.corpus.makeDictionary()
+        self.corpus.toString()
+
+    def getChain(self):
+        if(self.isParsed == True):
+            return "Markov chain here"
+        else:
+            return "Setup not Called!" 
+
+    def setup(self):
+        self.parseDictionary()
+        self.isParsed = True
+
+if __name__=="__main__":
+    markov = Markov("corpus.txt", 4)
+    markov.setup()
