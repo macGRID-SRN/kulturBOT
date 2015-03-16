b8973a90ce8ed15c21d96417292fbfb18357bafd
gagichce <gagichce@mcmaster.ca>
2015-03-16 00:18:11 -0400

    parser now removes characters, cleans up english numerals and superscript OCR mistakes

--- FormatJournal/FormatJournal/Program.fs
+++ FormatJournal/FormatJournal/Program.fs
@@ -1,26 +1,71 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+module Program
 
 open System
 open System.IO
+open System.Text
+open SuperScript
 
-let removeBadCharacters stringArr =
-    0
+//characters which should not be allowed
+let banishedChars = 
+    [|
+        '%';
+        ';';
+        '[';
+        ']';
+        '^';
+    |]
+
+let replacePhrases = 
+    [
+        ("M^'", "Mc");
+        ("M'^", "Mc");
+    ]
+
+//option to replace case.
+let matchCase = false;
+
+let removePhrase (text : string) = 
+    let mutable result = text
+    for i in replacePhrases do
+        result <- result.Replace(fst i, snd i)
+    result
+
+let removePhrases (text : string[]) = 
+    Array.map removePhrase text
+
+//removes all the unwanted characters
+let removeBadCharFromString (text : string) =
+    text.Split(banishedChars, StringSplitOptions.RemoveEmptyEntries) |> String.Concat
+
+//wrapper for string[]
+let removeBadCharacters (textArr : string[]) =
+    Array.map (fun (n : string) -> removeBadCharFromString n) textArr
 
 [<EntryPoint>]
 let main argv = 
-    let parse file =
+    let parse file out =
         try
             let toParse file =  
                 file  
                 |> File.ReadAllLines
+                |> findSuperScriptNum
+                |> removePhrases
+                |> removeBadCharacters
 
-            toParse file
-            with
-                | :? System.IO.FileNotFoundException -> printfn "File Name invalid."; [||]
+            File.WriteAllLines (out, toParse file)
+            true
+        with
+            | :? System.IO.FileNotFoundException -> printfn "File name invalid or file not found!"; false //returns empty string array.
 
+    let result = parse argv.[0] argv.[1]
 
-    ignore(parse "TXT.rtf")
-    Console.WriteLine "Opened File. Parsed Out."
+    let stringOutput = 
+        if result 
+        then "Opened File. Parsed Out." 
+        else "Something Failed."
+
+    Console.WriteLine stringOutput
     ignore(Console.ReadKey false)
     0 // return an integer exit code
---
+++ FormatJournal/FormatJournal/SuperScript.fs
@@ -0,0 +1,42 @@
+﻿module SuperScript
+open System
+open System.Linq
+
+let OCRmistakenChars = 
+    [
+        '\'';
+        '"';
+        '^';
+        '*';
+        '.';
+    ]
+
+let isNum s = 
+    match Int32.TryParse(s) with
+    |   (false, _) -> false
+    |   (true, _) -> true
+
+let isEnglishNumeral (s : string) i = 
+    (isNum (s.Substring(i, 1))) && 
+    (OCRmistakenChars.Contains (s.[i+1])) 
+    && 
+    ((OCRmistakenChars.Contains (s.[i+2])) || String.IsNullOrWhiteSpace (s.Substring(i+2, 1)))
+
+let getEnglishNumeral s = 
+    match s with
+    | '1' -> "st"
+    | '2' -> "nd"
+    | '3' -> "rd"
+    | _ -> "th"
+
+let containsNumber (text: string) = 
+    let mutable result = text
+    for i in [0 .. text.Length - 2] do
+        if isEnglishNumeral text i then
+          result <- [result.Substring(0,i+1); getEnglishNumeral result.[i]; result.Substring(i+3);] |> String.Concat
+    result
+            
+                
+
+let findSuperScriptNum (text : string[]) = 
+    Array.map containsNumber text
