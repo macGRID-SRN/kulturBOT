808e80dec97a20ace8cb8f5203c9e468059dcdac
gagichce <gagichce@mcmaster.ca>
2015-04-05 12:41:18 -0400

    various demo codes from netduino interfaceing

---
+++ kulturBOTnetduino/kulturBOTnetduino/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using Toolbox.NETMF.Hardware;
+using System.IO.Ports;
+
+namespace kulturBOT
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Thread.Sleep(1000);
+
+            iRobotTest();
+
+            while (true)
+            {
+                Thread.Sleep(100000);
+            }
+        }
+
+        public static void iRobotTest()
+        {
+            Thread.Sleep(2000);
+            var serial = new SerialPort(SerialPorts.COM2, 57600);
+
+            serial.Open();
+
+            serial.WriteByte(128);
+
+            serial.Write(new byte[] { 136, 0 }, 0, 2);
+            Thread.Sleep(15000);
+            serial.Write(new byte[] { 136, 255 }, 0, 2);
+
+            serial.Close();
+        }
+
+        //IO from the SD card breaks it :(
+        //public static string ReadLineFromSD()
+        //{
+        //    var file = new StreamReader(@"\SD\t.txt");
+
+        //    return file.ReadLine();
+
+        //    //return string.Empty;
+        //}
+
+        public static void PrinterTest()
+        {
+            Debug.Print("Setting up the printer!");
+            ThermalPrinter Printer = new ThermalPrinter();
+            Debug.Print("Printer is set up!");
+            // Use this to make the prints of better quality
+            // It will slow down the printer, and perhaps shorten the lifespan of the printer too
+            //Printer.HeatingTime = 255;
+            //Printer.HeatingInterval = 255;
+
+            // Font demo
+            Printer.PrintLine("Hello I am kulturBOT.");
+            //Printer.PrintLine("I like to write poems and make peoples day.");
+            //Printer.PrintLine("What do you think of me?");
+            Printer.PrintLine("@kulturBOT");
+            Printer.LineFeed(1);
+            Printer.Print(DateTime.UtcNow.ToString());
+            Printer.LineFeed(3);
+
+            Printer.Dispose();
+        }
+
+        public static void PrinterTest(string printText)
+        {
+            Debug.Print("Setting up the printer!");
+            ThermalPrinter Printer = new ThermalPrinter();
+            Debug.Print("Printer is set up!");
+            // Use this to make the prints of better quality
+            // It will slow down the printer, and perhaps shorten the lifespan of the printer too
+            //Printer.HeatingTime = 255;
+            //Printer.HeatingInterval = 255;
+
+            // Font demo
+            Printer.PrintLine(printText);
+            Printer.LineFeed(3);
+
+            Printer.Dispose();
+        }
+    }
+}
