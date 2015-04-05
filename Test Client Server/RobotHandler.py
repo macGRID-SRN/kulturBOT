587964fbe37435584ed01938458fa0a2fb74e0d7
gagichce <gagichce@mcmaster.ca>
2015-04-05 15:59:42 -0400

    gotta loca python documentation..

--- Test Client Server/RobotHandler.py
+++ Test Client Server/RobotHandler.py
@@ -1,9 +1,10 @@
 import serial
+import binascii
 
 class Netduino:
 	
 	def __init__(self):
-		self.serialport = serial.Serial("/dev/ttyAMA0", 57600, timeout=0.5)
+		self.serialport = serial.Serial("/dev/ttyAMA0", 57600, timeout=3.5)
 		self.serialport.write("\x80")
 		
 	def demo(self):
@@ -12,5 +13,5 @@ class Netduino:
 	def sendSentence(self, sentence):
 		b = sentence.encode('utf-8')
 		self.serialport.write("\x81")
-		self.serialport.write(bytes([len(b)]))
+		self.serialport.write(bytes(len(b)))
 		self.serialport.write(b)
