e40347d1436a34ae3ff07c2193f2c60366bbcc5e
gagichce <gagichce@mcmaster.ca>
2015-04-05 14:14:16 -0400

    updated logic flag

--- Test Client Server/debug.py
+++ Test Client Server/debug.py
@@ -1,7 +1,7 @@
 from sys import platform as _platform
 
 DEBUG = True
-if(_platform != "win32" && _platform != "darwin"):
+if(_platform != "win32" and _platform != "darwin"):
         DEBUG = False
 
 #Override the OS check
